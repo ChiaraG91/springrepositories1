6e5ad8a2acf89137b70cfa7c7bbc83c825b86fe2
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-26 23:33:23 +0100

    Add files via upload

---
+++ springrepositories1/src/main/java/com/springrepositories1/springrepositories1/Springrepositories1Application.java
@@ -0,0 +1,13 @@
+package com.springrepositories1.springrepositories1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrepositories1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrepositories1Application.class, args);
+	}
+
+}
